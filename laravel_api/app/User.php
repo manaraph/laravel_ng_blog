798ca58b1008051c00e69babcf5c4612964c5cef
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-12 16:03:22 +0100

    bugs on laravel side of things

--- laravel_api/app/User.php
+++ laravel_api/app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Laravek\Passport\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+    
+// Route::post('/login', 'Api\AuthController@login')->name('login.api');
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
