e3217b7dea29edd67ab6fc87c1d6ab4b2dfbc6a4
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-21 12:26:53 +0100

    Auth logic and tests

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
+Route::get('users', 'AuthController@users');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
