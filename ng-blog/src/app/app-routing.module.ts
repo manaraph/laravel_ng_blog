84c8f669185e7bc2cbdcb3f31f83c11957e01030
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-24 23:00:07 +0100

    Add auth guard

--- ng-blog/src/app/app-routing.module.ts
+++ ng-blog/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostDetailsComponent } from './posts/post-details/post-details.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'posts', component: PostsComponent },
+  { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] },
   { path: 'posts/:id', component: PostDetailsComponent },
   { path: '', redirectTo: '/posts', pathMatch: 'full' }
 ];
