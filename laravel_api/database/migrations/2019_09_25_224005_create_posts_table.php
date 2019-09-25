916ded488c58cc4ee20f31252a57a2ba0d0e8e77
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-26 00:36:01 +0100

    create post table

---
+++ laravel_api/database/migrations/2019_09_25_224005_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('post_name');
+            $table->string('date');
+            $table->string('time');
+            $table->integer('price');
+            $table->string('image_url');
+            $table->string('location')->nullable();
+            $table->string('online_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
