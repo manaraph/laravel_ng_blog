71c3da5cdbd31b0e6f5aa78d1c0208b18c94fd30
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-26 01:23:18 +0100

    Fix issues with models

--- ng-blog/src/app/_services/post.service.ts
+++ ng-blog/src/app/_services/post.service.ts
@@ -2,7 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Observable, of, Subject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-import { IEvent, ISession } from '../_models/post';
+import { IEvent } from '../_models/post';
 
 @Injectable({
   providedIn: 'root'
@@ -26,10 +26,6 @@ export class PostService {
      return this.http.post<IEvent>('/api/posts', post, options)
       .pipe(catchError(this.handleError<IEvent>('savePost')));
    }
-   searchSessions(searchTerm: string): Observable<ISession[]> {
-    return this.http.get<ISession[]>(`/api/sessions/search?search=${searchTerm}`)
-     .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
-  }
 
   private handleError<T>(operation = 'operation', result?: T) {
    return (error: any): Observable<T> => {
