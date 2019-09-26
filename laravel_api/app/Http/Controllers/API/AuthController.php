88221dca1c0596a088bd01524e8e45377e606524
Manasseh Omachonu <manaraph225@gmail.com>
2019-09-26 05:10:28 +0100

    AuthController => API\AuthController

---
+++ laravel_api/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\User;
+use Validator;
+
+class AuthController extends Controller
+{
+
+    /**
+     * user api
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function users()
+    {
+       return response()->json(['m4'=>'its working'], 200);
+    }
+
+    /**
+     * login api
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')]))
+        {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('myApp')->accessToken;
+            return response()->json(['success' => $success, 'message'=> 'Access granted'], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    /**
+     * register api
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        response()->json(['mdk'=> 'lsllsl'], 200);
+
+        $validator = Validator::make(
+            $request->all(), 
+            [
+                'name'=>'required', 
+                'email'=>'required|email', 
+                'password'=>'required', 
+                'confirmPassword'=>'required|same:password'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+            $input = $request->all();
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+            $success['token'] = $user->createToken('myApp')->accessToken;
+            $success['name'] = $user->name;
+            return response()->json(['success'=>$success, 'message'=> 'User created successfully'], 200);
+        
+    }
+}
